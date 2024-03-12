5b6216d43272ccf102b1f08e6fda080dd1ce9af4
Quinn Le <quynhnle135@gmail.com>
2024-03-12 14:12:16 -0400

    Added feature to Calculate Monthly Contribution

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Calculate compound interest with monthly contribution
 function calculateInterest() {
   const P = parseFloat(document.getElementById("initial").value);
   const C = parseFloat(document.getElementById("contribution").value);
@@ -14,4 +15,21 @@ function calculateInterest() {
     ) / 100;
 
   document.getElementById("result").innerText = `You will have: $${result}`;
+}
+
+// Calculate monthly contribution
+function calculateMonthlyContribution() {
+  const P = parseFloat(document.getElementById("principal").value);
+  const V = parseFloat(document.getElementById("expected").value);
+  let r = parseFloat(document.getElementById("rate").value);
+  r = r / 100 / 12;
+  const t = parseFloat(document.getElementById("period").value);
+  const n = 12 * t;
+
+  let result = Math.round(
+    (V - P * Math.pow(1 + r, n)) / ((Math.pow(1 + r, n) - 1) / r)
+  );
+  document.getElementById(
+    "monthlyContribution"
+  ).innerText = `Monthly Contribution: $${result}`;
 }
